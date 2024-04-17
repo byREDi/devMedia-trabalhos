9004c2fb3f0e13537e8502a60150d20ac857df5b
Augusto Henrique <augusto_henri1@hotmail.com>
2024-04-16 22:29:54 -0300

    Finalizando o card solicitado
    Finalizei o "card" que foi passado como trabalho final nesse módulo do curso DevMedia, sem consultar os auxílios.

--- trabalho-final/App.js
+++ trabalho-final/App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import BackgroundImg from './assets/background.jpg';
+import UserImg from './assets/user.jpg';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={ styles.backgroundCard }>
+        <Image style={ styles.imgFundo } source={ BackgroundImg } />
+
+        <View style={ styles.cardUser }>
+          <Image style={ styles.userImg } source={ UserImg } />
+          <Text style={ styles.nameUser }>Augusto Henrique</Text>
+          <Text style={ styles.textUser }>Estudando apaixonado por tecnologia, terminou o curso técnico de ADS no fim de 2020 e está estudando diáriamente para conseguir sua primeira vaga como dev mobile.</Text>
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  cardUser: {
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+
+  nameUser: {
+    fontSize: 22,
+    marginTop: 20,
+    fontWeight: 'bold',
+  },
+
+  textUser: {
+    textAlign: 'center',
+    marginTop: 10,
+  },
+
+  imgFundo: {
+    width: 400,
+    height: 350,
+  },
+
+  userImg: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  }
 });

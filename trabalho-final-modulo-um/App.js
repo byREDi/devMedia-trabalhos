7f97e53e20d3c2f291d345d8e1ee96259c4bfcd7
Augusto Henrique <augusto_henri1@hotmail.com>
2024-04-16 22:56:07 -0300

    Atualizando componentes

--- trabalho-final-modulo-um/App.js
+++ trabalho-final-modulo-um/App.js
@@ -6,45 +6,51 @@ import UserImg from './assets/user.jpg';
 
 export default function App() {
   return (
-      <View style={ styles.backgroundCard }>
-        <Image style={ styles.imgFundo } source={ BackgroundImg } />
+    <View style={ styles.container }>
+        <Image style={ styles.fotoTopo } source={ BackgroundImg } />
 
-        <View style={ styles.cardUser }>
-          <Image style={ styles.userImg } source={ UserImg } />
-          <Text style={ styles.nameUser }>Augusto Henrique</Text>
-          <Text style={ styles.textUser }>Estudando apaixonado por tecnologia, terminou o curso técnico de ADS no fim de 2020 e está estudando diáriamente para conseguir sua primeira vaga como dev mobile.</Text>
-        </View>
+      <View style={ styles.boxDescricao }>
+        <Image style={ styles.userImg } source={ UserImg } />
+        <Text style={ styles.nameUser }>Augusto Henrique</Text>
+        <Text style={ styles.textUser }>Estudando apaixonado por tecnologia, terminou o curso técnico de ADS no fim de 2020 e está estudando diáriamente para conseguir sua primeira vaga como dev mobile.</Text>
       </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  cardUser: {
-    backgroundColor: '#fff',
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+
+  fotoTopo: {
+    flex: 1,
+  },
+
+  boxDescricao: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+  },
+
+  userImg: {
+    marginTop: 20,
+    marginBottom: 20,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 
   nameUser: {
     fontSize: 22,
-    marginTop: 20,
+    marginBottom: 20,
     fontWeight: 'bold',
   },
 
   textUser: {
     textAlign: 'center',
-    marginTop: 10,
+    fontSize: 16,
+    marginHorizontal: 15,
+    padding: 10,
   },
-
-  imgFundo: {
-    width: 400,
-    height: 350,
-  },
-
-  userImg: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-  }
 });

827a41076e122ee81d44c450ea9a15bd01e561f6
Augusto Henrique <augusto_henri1@hotmail.com>
2024-04-22 18:34:01 -0300

    Navegação entre páginas
    Aprendendo mais sobre navegação entre páginas usando a biblioteca react-navigation/native e seus 3 métodos.
    Tab, Drawer e Stack.

---
+++ navegacao-entre-pages/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import TelaInicial from "./Componentes/TelaInicial/";
+import TelaPerfil from "./Componentes/TelaPerfil/";
+
+const Drawer = createDrawerNavigator();
+
+export default function App () {
+  return (
+    <NavigationContainer >
+      <Drawer.Navigator>
+        <Drawer.Screen name="Tela Inicial" component = { TelaInicial } />
+        <Drawer.Screen name="Tela Perfil" component = { TelaPerfil } />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+};

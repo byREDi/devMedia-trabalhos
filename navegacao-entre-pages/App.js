aeaff468e4c71463cf6951734c8d18328d600efd
Augusto Henrique <augusto_henri1@hotmail.com>
2024-04-22 18:49:43 -0300

    Button para abrir menu lateral
    Usando o componente Button para abrir o menu lateral.

--- navegacao-entre-pages/App.js
+++ navegacao-entre-pages/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
